367bacab7b7152911d31aa30c387843efa514553
Paul Santiago <paulsantiago282@gmail.com>
2023-08-04 18:58:14 -0400

    Add example of binding a directive to a property with the input decorator

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,24 +1,25 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input() defaultColor: string = 'transparent'
+  @Input() highlightColor: string = 'blue'
    // basically saying: on element that directive sits, please access the style property and set it to whatever value you want
    @HostBinding('style.backgroundColor') backgroundColor: string;
 
   // listens to an event that's defined in the dom mouseenter and mouseleave are two events supported byt the dom element that this directive sits on
   @HostListener('mouseenter') mouseover(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
-
 
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
@@ -26,8 +27,8 @@ export class BetterHighlightDirective implements OnInit {
    }
    // takes a specific element like <p> <div> and updates the background color for it. a better practice as far as using directives better approach of accessing the dom
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor; // resolves bug of having background color initiall be transparent
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
-     this.renderer.setStyle(this.elRef.nativeElement, 'color','white')
   }
 
 }
