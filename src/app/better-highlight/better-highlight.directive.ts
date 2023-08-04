cb1991c174499b006308128aebe9c866c6de8084
Paul Santiago <paulsantiago282@gmail.com>
2023-08-04 18:33:05 -0400

    Add host listener example for hovering over using a mouse

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,17 +1,27 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // listens to an event that's defined in the dom
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+
+  }
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
    }
    // takes a specific element like <p> <div> and updates the background color for it. a better practice as far as using directives better approach of accessing the dom
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
-    this.renderer.setStyle(this.elRef.nativeElement, 'color','white')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color','white')
   }
 
 }
